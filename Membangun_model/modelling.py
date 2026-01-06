0c82073d5eabab707d9ccde9dbe0e77e4de861f5
Nayla <naaylaalifah@gmail.com>
2026-01-06 23:16:51 +0700

    fix modelling.py

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -18,33 +18,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("AirQuality_Baseline_Model")
 
+mlflow.sklearn.autolog()
+
 with mlflow.start_run():
 
     model = RandomForestRegressor(
-        n_estimators=150,
+        n_estimators=100,
         random_state=42
     )
 
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    rmse = mean_squared_error(y_test, y_pred) ** 0.5
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-
-    mlflow.log_param("model_type", "RandomForestRegressor")
-    mlflow.log_param("n_estimators", 150)
-
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("mae", mae)
-    mlflow.log_metric("r2_score", r2)
-
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model",
-        input_example=X_test.iloc[:5]
-    )
-
-    print(f"RMSE: {rmse}")
-    print(f"MAE: {mae}")
-    print(f"R2 Score: {r2}")
